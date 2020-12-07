853c93f8d7f94201e839274037fed404b658f03b
manikiranp <manikiranp3@gmail.com>
2020-12-07 14:10:28 +0530

    Added: Test case for Pincode regex pattern

--- Regextest.js
+++ Regextest.js
@@ -1,6 +1,6 @@
-var prompt = require('prompt-sync')();	
-console.log("Regex Test:");
-var inp=prompt("Enter Pincode: ");
+
+console.log("Regex Test Pincode:");
+var test1="400088";
 let regex = new RegExp('^[1-9][0 -9]{5}$');
-var validation = regex.test(inp);
-console.log(inp+" "+validation);
+var validation = regex.test(test1);
+console.log(test1+" "+validation); 
